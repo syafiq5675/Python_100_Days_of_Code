6816521c9c0af7fa644adca21c7a74570a265b95
Syafiq <hmohamadsyafiq@gmail.com>
2026-05-06 23:03:28 +0800

    Finish flow from start to finish without logic

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -12,6 +12,18 @@ def count_value(cards):
         total += card
     return total
 
+def decide_winner(user, comp):
+    user_value = count_value(user)
+    comp_value = count_value(comp)
+    print(f"     Your final hand: {user}, final score: {user_value}\n"
+          f"     Computer's final hand: {comp}, final score: {comp_value}")
+    if user_value > comp_value:
+        print("You win")
+    elif user_value == comp_value:
+        print("Draw")
+    elif comp_value > user_value:
+        print("You lose")
+
 def blackjack():
     play_blackjack = input("Do you want to play a game of Blackjack? type 'y' or 'n':  ")
     if play_blackjack == "y":
@@ -33,3 +45,12 @@ def blackjack():
     print(f"     Your cards: {user_hand}, current score: {count_value(user_hand)}\n"
           f"     Computer's first card: {dealer_hand}")
 
+    give_card = True
+    get_card = input("Type 'y' to get another card, type 'n' to pass: ")
+    if get_card == 'n':
+        decide_winner(user_hand, dealer_hand)
+    elif get_card == 'y':
+        deal_card(user_hand)
+        get_card = input("Type 'y' to get another card, type 'n' to pass: ")
+
+blackjack()
