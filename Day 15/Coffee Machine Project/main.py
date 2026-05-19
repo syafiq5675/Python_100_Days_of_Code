2977a342820f267000bce2094a734f760905a3c3
Syafiq <hmohamadsyafiq@gmail.com>
2026-05-19 08:16:41 +0800

    D15 - Make coffee function halfway completed

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,3 +1,5 @@
+from pickle import GLOBAL
+
 from source import MENU as MENU
 
 ready = True
@@ -41,19 +43,24 @@ def process_coins():
     pennies = int(input('How many pennies? : ')) * 0.01
     return quarters + dimes + nickles + pennies
 
-paid = process_coins()
-print(paid)
-
 # 4.  store in money , return the balance if needed
 
-def process_transactions(price):
-    pass
-
+def process_transactions(drink, payment, money):
+    price = MENU[drink]['cost']
+    money += price
+    return payment - price
 
 # 5. Make the coffee. Deduct the resources that is used, return the coffee and print Here is your {drink}. Enjoy!
 
-def make_coffee(drink):
-    pass
+def make_coffee(drink, balance):
+    price = MENU[drink]['cost']
+    if balance > price or balance == price:
+        print(f"Here is ${balance} dollars in change")
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
 
 # 6. Apart from the drink, make option to print report and switch off at the input
 
